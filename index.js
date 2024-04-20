e199f5eb8442906c336b3648eb66068fb77d6f00
ajithasamuel38 <ajithasamuel38@gamil.com>
2024-04-20 22:20:04 +0530

    expensenode

---
+++ index.js
@@ -0,0 +1,31 @@
+const link = "http://localhost:3000/user/signup";
+const form = document.getElementById('signup');
+form.addEventListener('submit', postuserdetails);
+
+
+async function postuserdetails(event){
+    event.preventDefault();
+    let name = event.target.name.value;
+    let email = event.target.email.value;
+    let password = event.target.password.value;
+    let postObj = {
+        name:name,
+        email:email,
+        password:password
+    };
+    try{
+        const response = await axios.post(link, postObj);
+       
+        console.log(response);
+      
+    }
+    catch(err){
+        console.log(err);
+        clearForm();
+    }
+    
+}
+
+function clearForm() {
+    document.getElementById("signup").reset();
+  }
